2f1323ef1b4f20f2e8f1a60af33cd5f146ca1da9
Rafael Epplée <hello@rafa.ee>
2026-09-25 21:39:17 +0200

    do stuff

--- player.gd
+++ player.gd
@@ -2,22 +2,22 @@ extends Node3D
 
 var input_direction = Vector3.ZERO
 
-@onready var body := $RigidBody3D
-@onready var shrimp := $"RigidBody3D/shrimp"
+@onready var body := $body
+@onready var mesh := $mesh
 var ground_speed = 10
+@onready var camera_pivot := $CameraPivot
 
 
 func process_air(delta: float) -> void:
-    var next_position = self.global_position - transform.basis.z
-    shrimp.look_at(next_position)
+    var next_position = self.global_position + body.linear_velocity
+    mesh.look_at(next_position)
     # glide forward
-    body.apply_central_force(-transform.basis.z * delta * 200)
+    body.apply_central_force(-mesh.transform.basis.z * delta * 200)
     # reduce gravity
-    body.apply_central_force(transform.basis.y * delta * 300)
+    body.apply_central_force(Vector3.UP * delta * 400)
 
 
 func process_ground(_delta: float) -> void:
-    print(input_direction.y)
     var movement = input_direction * ground_speed
     body.apply_central_force(Vector3(movement.x, 0, movement.y))
     return
@@ -30,6 +30,7 @@ func get_input_direction() -> void:
 func _physics_process(delta: float) -> void:
     # TODO: Find out whether we are on ground
     get_input_direction()
-    $CameraPivot.global_position = body.global_position
+    camera_pivot.global_position = body.global_position
+    mesh.global_position = body.global_position
     # process_air(delta)
     process_ground(delta)
