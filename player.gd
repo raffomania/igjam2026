e9d585df05fb36ab9409339751e2e3041689ae90
Rafael Epplée <hello@rafa.ee>
2026-09-25 21:57:31 +0200

    toggle flying mode

--- player.gd
+++ player.gd
@@ -6,15 +6,20 @@ var input_direction = Vector3.ZERO
 @onready var mesh := $"body/mesh"
 var ground_speed = 10
 @onready var camera_pivot := $CameraPivot
+var flying_mode := false
 
 
 func process_air(delta: float) -> void:
-    var next_position = self.global_position + body.linear_velocity
-    mesh.look_at(next_position)
     # glide forward
-    body.apply_central_force(-mesh.transform.basis.z * delta * 200)
+    body.apply_central_force(-body.transform.basis.z * delta * 400)
     # reduce gravity
     body.apply_central_force(Vector3.UP * delta * 400)
+    var up_rotation = input_direction.y
+    if up_rotation >= 0.0:
+        var max_x_basis = Vector3.UP
+        var distance_from_max_x_basis = max_x_basis.angle_to(-body.transform.basis.z)
+        print(distance_from_max_x_basis)
+        body.apply_torque(Vector3.RIGHT * delta * up_rotation * distance_from_max_x_basis * 5)
 
 
 func process_ground(_delta: float) -> void:
@@ -32,5 +37,13 @@ func _physics_process(delta: float) -> void:
     get_input_direction()
     camera_pivot.global_position = body.global_position
     mesh.global_position = body.global_position
-    # process_air(delta)
-    process_ground(delta)
+    process_air(delta)
+    # process_ground(delta)
+
+func _unhandled_input(event: InputEvent) -> void:
+    if event.is_action_released("toggle_flying"):
+        flying_mode = !flying_mode
+        if flying_mode:
+            mesh.scale.x = 2
+        else:
+            mesh.scale.x = 1
