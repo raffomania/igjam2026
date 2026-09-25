45984cb8ee47e3dfaeba90bb3a0e99f1bf27e42e
Rafael Epplée <hello@rafa.ee>
2026-09-25 20:22:18 +0200

    process player

--- player.gd
+++ player.gd
@@ -4,10 +4,15 @@ var input_direction = Vector3.ZERO
 
 
 func process_air(delta: float) -> void:
-    return
+    var next_position = self.position - transform.basis.z
+    $shrimp.look_at(next_position)
+    # glide forward
+    self.apply_central_force(-transform.basis.z * delta * 200)
+    # reduce gravity
+    self.apply_central_force(transform.basis.y * delta * 400)
 
 
-func process_ground(delta: float) -> void:
+func process_ground(_delta: float) -> void:
     return
 
 
@@ -22,6 +27,10 @@ func get_input_direction() -> void:
         input_direction.z -= 1
 
 
-func _process(delta: float) -> void:
+func _ready() -> void:
+    self.apply_central_force(Vector3.FORWARD * 100)
+
+
+func _physics_process(delta: float) -> void:
     # TODO: Find out whether we are on ground
-    process_ground(delta)
+    process_air(delta)
