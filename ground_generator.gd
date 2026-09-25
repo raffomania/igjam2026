14ca47a3ce3b375b37cd9848322c5c839c7b2a46
Karim Djemai <karimdjemai1997@gmail.com>
2026-09-25 21:02:53 +0200

    loop over grid

--- ground_generator.gd
+++ ground_generator.gd
@@ -1,7 +1,7 @@
 extends Node
 
-var resolution = 20 # vertex per meter in all directions
-var size = 10 # meters
+var resolution = 4 # vertex per meter in all directions
+var size = 1 # meters
 var vertices_per_dimension = resolution * size
 var grid_vertex_distance = 1.0 / vertices_per_dimension #meters
 
@@ -17,12 +17,7 @@ func _ready():
 
     verts = create_grid_vertices()
     indices = create_grid_indices()
-    normals = PackedVector3Array([
-        Vector3.UP,
-        Vector3.UP,
-        Vector3.UP,
-        Vector3.UP,
-    ])
+    normals =  create_normals()
     # uvs = PackedVector2Array([
     #     Vector2(0, 0),
     #     Vector2(1, 0),
@@ -44,40 +39,52 @@ func _ready():
     add_child(m)
 
 func create_grid_vertices():
-    var verts = PackedVector3Array([
-        Vector3(0, 0, 0),
-        Vector3(0, 0, 1),
-        Vector3(1, 0, 0),
-        Vector3(1, 0, 1),
-    ])
-    return verts
+    var grid_vertices = PackedVector3Array()
+    for x in range(vertices_per_dimension):
+        for z in range(vertices_per_dimension):
+            #TODO: calculate y based on sin functions
+            var y = 0
+            var vertex_position = Vector3(x*grid_vertex_distance,y,z*grid_vertex_distance)
+            grid_vertices.push_back(vertex_position)
+    # print("grid_vertices", grid_vertices)
+    return grid_vertices
 
 func create_grid_indices():
-    var indices = PackedInt32Array([
-        0, 2, 1, # Draw the first triangle.
-        2, 3, 1, # Draw the second triangle.
-    ])
+    var indices = PackedInt32Array()
+    var i = 0
+    for x in range(vertices_per_dimension):
+        for z in range(vertices_per_dimension):
+            indices.push_back(i+vertices_per_dimension)
+            indices.push_back(i+1)
+            indices.push_back(i)
+            indices.push_back(i+vertices_per_dimension)
+            indices.push_back(i+vertices_per_dimension+1)
+            indices.push_back(i+1)
+            i += 1
+        i += 1
+    # print("indices", indices)
     return indices
+
+func create_normals():
+    var normals = PackedVector3Array()
+    for n in range(vertices_per_dimension*vertices_per_dimension):
+        normals.push_back(Vector3.UP)
+    return normals
+
 # var ground_vertices = PackedVector3Array()
 #
 # func _init() -> void:
-# 	print("lol")
+#     print("lol")
 #
-# 	for x in range(size):
-# 		for z in range(size):
-# 			#TODO: calculate y based on sin functions
-# 			var y = 0
-# 			var vertex_position = Vector3(x,y,z)
-# 			ground_vertices.push_back(vertex_position)
 #
-# 	print("finished")
-# 	var arr_mesh = ArrayMesh.new()
-# 	var arrays = []
-# 	arrays.resize(Mesh.ARRAY_MAX)
-# 	arrays[Mesh.ARRAY_VERTEX] = ground_vertices
+#     print("finished")
+#     var arr_mesh = ArrayMesh.new()
+#     var arrays = []
+#     arrays.resize(Mesh.ARRAY_MAX)
+#     arrays[Mesh.ARRAY_VERTEX] = ground_vertices
 #
 # # Create the Mesh.
-# 	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
-# 	var m = MeshInstance3D.new()
-# 	m.mesh = arr_mesh
-# 	add_child(m)
+#     arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+#     var m = MeshInstance3D.new()
+#     m.mesh = arr_mesh
+#     add_child(m)
